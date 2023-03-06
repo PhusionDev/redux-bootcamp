38b7a40b27f3877531fc4dc29f55a2e960f575df
Jared Hodgkins <PhusionDev@gmail.com>
2023-03-06 10:15:00 -0500

    34. Combine Reducers

--- redux-only/post-app/posts.js
+++ redux-only/post-app/posts.js
@@ -1,15 +1,23 @@
-const { createStore } = require('redux');
+const { createStore, combineReducers } = require('redux');
 
 // constants
+// post constants
 const ADD_POST = 'ADD_POST';
 const REMOVE_POST = 'REMOVE_POST';
+// user constants
+const ADD_USER = 'ADD_USER';
 
 // initial state
 const initialState = {
   posts: [],
 };
 
+const usersInitialState = {
+  users: [],
+};
+
 // actions (action creators)
+// post actions
 const addPostAction = (post) => {
   return {
     type: ADD_POST,
@@ -24,7 +32,16 @@ const removePostAction = (id) => {
   };
 };
 
-// reducer
+// user actions
+const addUserAction = (user) => {
+  return {
+    type: ADD_USER,
+    payload: user,
+  };
+};
+
+// reducers
+// posts reducer
 const postReducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_POST:
@@ -42,13 +59,32 @@ const postReducer = (state = initialState, action) => {
   }
 };
 
+// users reducer
+const userReducer = (state = usersInitialState, action) => {
+  switch (action.type) {
+    case ADD_USER:
+      return {
+        users: [...state.users, action.payload],
+      };
+    default:
+      return state;
+  }
+};
+
+// root reducer
+const rootReducer = combineReducers({
+  posts: postReducer,
+  users: userReducer,
+});
+
 // store
-const store = createStore(postReducer);
+const store = createStore(rootReducer);
 
 // subscribe
 store.subscribe(() => {
   const data = store.getState();
-  console.log(data);
+  console.log('posts', data.posts);
+  console.log('users', data.users);
 });
 
 // dispatch
@@ -58,3 +94,6 @@ store.dispatch(addPostAction({ id: 2, title: 'How to master redux' }));
 
 // remove post
 store.dispatch(removePostAction(1));
+
+// add new user
+store.dispatch(addUserAction({ name: 'John', email: 'john@example.com' }));
