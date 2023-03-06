111f93cb58f9bc779dd6b046747d6a8d62634ac9
Jared Hodgkins <PhusionDev@gmail.com>
2023-03-06 08:32:20 -0500

    31. Project Setup Part 2

--- redux-only/post-app/posts.js
+++ redux-only/post-app/posts.js
@@ -20,13 +20,41 @@ const addPostAction = (post) => {
 const removePostAction = (id) => {
   return {
     type: REMOVE_POST,
+    payload: id,
   };
 };
 
 // reducer
-const postReducer = (state = initialState, action) => {};
+const postReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ADD_POST:
+      return {
+        posts: [...state.posts, action.payload],
+      };
+    case REMOVE_POST:
+      return {
+        posts: state.posts.filter((post) => {
+          return post.id !== action.payload;
+        }),
+      };
+    default:
+      return state;
+  }
+};
 
 // store
 const store = createStore(postReducer);
 
+// subscribe
+store.subscribe(() => {
+  const data = store.getState();
+  console.log(data);
+});
+
 // dispatch
+// add post action
+store.dispatch(addPostAction({ id: 1, title: 'Best Course' }));
+store.dispatch(addPostAction({ id: 2, title: 'How to master redux' }));
+
+// remove post
+store.dispatch(removePostAction(1));
